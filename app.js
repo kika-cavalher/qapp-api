b6089d92d8c2ac7b83deac9848b118e2794b33d8
QA Tropix <95445019+qa-tropix@users.noreply.github.com>
2022-07-22 14:37:50 -0300

    private route

--- app.js
+++ app.js
@@ -7,10 +7,44 @@ const jwt = require('jsonwebtoken')
 const app = express()
 app.use(express.json())
 
+//PUBLIC
+
 app.get('/', (req, res) => {
     res.status(200)
     .json({msg: 'Bem vindo a nossa API'})
 })
+
+//PRIVATE
+
+app.get('/user/:id', chectToken, async (req, res) => {
+    const id = req.params.id
+
+    const user = await User.findById(id, '-password')
+
+    if(!user) {
+        return res.status(404).json({msg: 'O usuario não encontrado'})
+    }
+
+    res.status(200).json({ user })
+})
+
+function chectToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token) {
+        return res.status(401).json({msg: "Acesso negado"})
+    }
+
+    try {
+        const secret = process.env.secret
+        jwt.verify(token, secret)
+        next()
+
+    } catch(err) {
+        return res.status(400).json({msg: 'Token inválido'})
+    }
+}
 
 //register
 
@@ -99,6 +133,7 @@ app.post('/auth/signin', async(req, res) => {
     }
 
 })
+
 
 const dbUser=  process.env.DB_USER
 const dbPass= process.env.DB_PASS
