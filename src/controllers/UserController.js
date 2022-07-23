54765097bf7e5c3d4a7e5ee5a9631a02b7f84698
QA Tropix <95445019+qa-tropix@users.noreply.github.com>
2022-07-22 22:56:33 -0300

    create function login

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,27 +6,27 @@ const createUserToken = require('../helpers/create-user-token');
 module.exports = class UserController {
 
     static async register(req, res) {
-        const { name, email, password, confirmPassword} = req.body
-        
-        if(!name) {
-            return res.status(422).json({msg: 'O nome deve ser obrigatório'})
+        const { name, email, password, confirmPassword } = req.body
+
+        if (!name) {
+            return res.status(422).json({ msg: 'O nome deve ser obrigatório' })
         }
-        if(!email) {
-            return res.status(422).json({msg: 'O e-mail deve ser obrigatório'})
+        if (!email) {
+            return res.status(422).json({ msg: 'O e-mail deve ser obrigatório' })
         }
-        if(!password) {
-            return res.status(422).json({msg: 'A senha deve ser obrigatório'})
+        if (!password) {
+            return res.status(422).json({ msg: 'A senha deve ser obrigatório' })
         }
-        if(!confirmPassword) {
-            return res.status(422).json({msg: 'A confirmação de senha deve ser obrigatório'})
+        if (!confirmPassword) {
+            return res.status(422).json({ msg: 'A confirmação de senha deve ser obrigatório' })
         }
-        if(password !== confirmPassword ) {
-            return res.status(422).json({msg: 'A senha deve ser igual a informada'})
+        if (password !== confirmPassword) {
+            return res.status(422).json({ msg: 'A senha deve ser igual a informada' })
         }
 
         const userExist = await User.findOne({ email: email })
-        if(userExist) {
-            return res.status(422).json({msg: 'Utilize outro e-mail, esse já existe no banco.'})
+        if (userExist) {
+            return res.status(422).json({ msg: 'Utilize outro e-mail, esse já existe no banco.' })
         }
 
         const salt = await bcrypt.genSalt(12)
@@ -36,13 +36,36 @@ module.exports = class UserController {
             email,
             password: passwordHash,
         })
-    
-        try{
+
+        try {
             const newUser = await user.save()
             await createUserToken(newUser, req, res)
-        
-        }catch(err){
-            res.status(500).json({msg: err})
+
+        } catch (err) {
+            res.status(500).json({ msg: err })
         }
+    }
+
+    static async login(req, res) {
+        const { email, password } = req.body
+        if (!email) {
+            return res.status(422).json({ msg: 'O e-mail deve ser obrigatório' })
+        }
+        if (!password) {
+            return res.status(422).json({ msg: 'A senha deve ser obrigatório' })
+        }
+
+        const user = await User.findOne({ email: email })
+        if (!user) {
+            return res.status(422).json({ msg: 'Esse user não existe no banco.' })
+        }
+
+        const checkPassword = await bcrypt.compare(password, user.password)
+        if (!checkPassword) {
+            return res.status(422).json({ msg: 'Senha inválida' })
+        }
+
+        await createUserToken(user, req, res)
+
     }
 }
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 
 router.post('/signup', UserController.register)
+router.post('/signin', UserController.login)
 
 module.exports = router;
