230e23e6d0c2a1e9384292352f12aaae07dd9f8a
QA Tropix <95445019+qa-tropix@users.noreply.github.com>
2022-07-30 17:44:44 -0300

    private routes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -95,4 +95,11 @@ module.exports = class UserController {
        }
         res.status(200).json({ user })
     };
+
+    static async editUser(req, res){
+        res.status(200).json({ msg: 'ok cuzãooo' })
+        return 
+    };
+
+
 }
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 
+const verifyToken = require('../helpers/verify-token')
+
 router.post('/signup', UserController.register)
 router.post('/signin', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', verifyToken, UserController.editUser)
 
 module.exports = router;
