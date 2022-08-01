46213b3dd3dd9608cb232a7428439a880af062be
QA Tropix <95445019+qa-tropix@users.noreply.github.com>
2022-08-01 18:22:00 -0300

    Atlas

--- src/db/comn.js
+++ src/db/comn.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/qappApi')
+function main() {
+  const dbUrl = process.env.NODE_ENV === 'production' ? process.env.DB_PROD : process.env.DB_LOCAL;
+  mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   console.log('Conectado ao MongoDb')
 }
 
-main().catch((err) => console.log(err))
+main()
 
 
 module.exports = mongoose;
