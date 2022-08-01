f489ebe14f11bd92044fd47df1a9ad5cc3da7f40
QA Tropix <95445019+qa-tropix@users.noreply.github.com>
2022-08-01 19:22:22 -0300

    finish mongo

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ const express = require('express');
 const requireDir = require('require-dir')
 const cors = require('cors');
 const UserRoutes = require('./src/routes/UserRoutes')
-// require('./src/db/comn');
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-(async () => {
-    try {
-        const dbUrl = process.env.NODE_ENV === 'production' ? process.env.DB_PROD : process.env.DB_LOCAL;
-        await db.mongoose.connect(dbUrl, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Conectado ao banco de dados');
-    } catch (error) {
-        console.log(`Erro ao conectar no banco de dados! ${error}`);
+try {
+    const dbUrl = process.env.NODE_ENV === 'production' ? process.env.DB_PROD : process.env.DB_LOCAL;
+    mongoose.connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log('Conectado ao banco de dados');
+    console.log(dbUrl)
+} catch (error) {
+    console.log(`Erro ao conectar no banco de dados! ${error}`);
 
-        process.exit();
-    }
-})();
+    process.exit();
+}
+
 
 //Inicialized
 const app = express();
@@ -33,7 +34,7 @@ requireDir('./src/models')
 // Public folder for img
 app.get('/', (req, res) => {
     res.status(200)
-    .json({msg: 'Bem vindo a nossa API'})
+        .json({ msg: 'Bem vindo a nossa API' })
 })
 
 //Routes
